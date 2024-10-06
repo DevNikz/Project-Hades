a168d8b55ec2bfd5bee7659c2af15650a5ebe516
DevNikz <paul_ragudo@dlsu.edu.ph>
2024-10-06 20:26:11 +0800

    Pillar Hazard thing implemented

--- Assets/Scripts/Hazard/HazardController.cs
+++ Assets/Scripts/Hazard/HazardController.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -12,8 +11,15 @@ public class HazardController : MonoBehaviour
     [BoxGroup("Properties/box1", ShowLabel = false)]
     public float entitySpeed;
 
-    [PropertySpace, TitleGroup("Hazards", "Specific Hazard Properties", TitleAlignments.Centered)]
+    [PropertySpace, TitleGroup("Timer", "General Timer Properties", TitleAlignments.Centered)]
+    [BoxGroup("Timer/Settings", ShowLabel = false)]
+    [ReadOnly, SerializeReference] protected TimerState timerState = TimerState.None;
+    [BoxGroup("Timer/Settings", ShowLabel = false)]
+    [Range(0.1f, 10f)] public float curTimer = 1f;
+    [BoxGroup("Timer/Settings", ShowLabel = false)]
+    [ReadOnly] public float tempTimer = 0f;
 
+    [PropertySpace, TitleGroup("Hazards", "Specific Hazard Properties", TitleAlignments.Centered)]
     public bool EnableBog;
     [ShowIfGroup("EnableBog")]
     [BoxGroup("EnableBog/Bog", ShowLabel = false)]
@@ -23,6 +29,12 @@ public class HazardController : MonoBehaviour
     [ShowIfGroup("EnablePillar")]
     [BoxGroup("EnablePillar/Pillar", ShowLabel = false)]
     [ReadOnly, SerializeReference] protected int counter = 1;
+
+    [BoxGroup("EnablePillar/Pillar", ShowLabel = false)]
+    [ReadOnly, SerializeReference] protected ParticleSystem hitEffect;
+
+    [BoxGroup("EnablePillar/Pillar", ShowLabel = false)]
+    [ReadOnly, SerializeReference] protected ParticleSystem shakeEffect;
 
     [BoxGroup("EnablePillar/Pillar", ShowLabel = false)]
     [ReadOnly, SerializeReference] protected GameObject pillar1;
@@ -42,12 +54,41 @@ public class HazardController : MonoBehaviour
 
     void Awake() {
         if(hazardType == HazardType.Pillar) {
+            hitEffect = transform.Find("HitEffect").GetComponent<ParticleSystem>();
+            shakeEffect = transform.Find("ShakeEffect").GetComponent<ParticleSystem>();
             pillar1 = transform.Find("1").gameObject;
             pillar2 = transform.Find("2").gameObject;
             pillar3 = transform.Find("3").gameObject;
             pillar4 = transform.Find("4").gameObject;
             pillar5 = transform.Find("5").gameObject;
         }
+    }
+
+    void Update() {
+        SwitchSpritePillar();
+        UpdateTimer();
+    }
+
+    void UpdateTimer() {
+        if(timerState == TimerState.Start) {
+            tempTimer -= Time.deltaTime;
+            if(tempTimer <= 0.0f) {
+                timerState = TimerState.Stop;
+            }
+        }
+
+        else if(timerState == TimerState.Stop) {
+            pillar1.SetActive(false);
+            pillar2.SetActive(false);
+            pillar3.SetActive(false);
+            pillar4.SetActive(false);
+            pillar5.SetActive(true);
+
+            shakeEffect.Stop();
+            tempTimer = 0f;
+            timerState = TimerState.None;
+        }
+        
     }
 
     public void InitHazard(GameObject gameObject = null) {
@@ -75,48 +116,55 @@ public class HazardController : MonoBehaviour
     }
 
     void InitPillar() {
+        //Play Effect
+        hitEffect.Play();
+
+        //Add Counter
         counter += 1;
-        if(counter > 5) counter = 5;
-        SwitchSpritePillar();
+        if (counter == 11) {
+            Debug.Log("Shaking!");
+            shakeEffect.Play();
+            tempTimer = curTimer;
+            timerState = TimerState.Start;
+        }
+        else if(counter > 11) counter = 11;
     }
 
     void SwitchSpritePillar() {
         switch(counter) {
-            case 1:
+            case 3:
                 pillar1.SetActive(true);
                 pillar2.SetActive(false);
                 pillar3.SetActive(false);
                 pillar4.SetActive(false);
                 pillar5.SetActive(false);
                 break;
-            case 2:
+            case 5:
                 pillar1.SetActive(false);
                 pillar2.SetActive(true);
                 pillar3.SetActive(false);
                 pillar4.SetActive(false);
                 pillar5.SetActive(false);
                 break;
-            case 3:
+            case 7:
                 pillar1.SetActive(false);
                 pillar2.SetActive(false);
                 pillar3.SetActive(true);
                 pillar4.SetActive(false);
                 pillar5.SetActive(false);
                 break;
-            case 4:
+            case 9:
                 pillar1.SetActive(false);
                 pillar2.SetActive(false);
                 pillar3.SetActive(false);
                 pillar4.SetActive(true);
                 pillar5.SetActive(false);
                 break;
-            case 5:
-                pillar1.SetActive(false);
-                pillar2.SetActive(false);
-                pillar3.SetActive(false);
-                pillar4.SetActive(false);
-                pillar5.SetActive(true);
-                break;
+            // case 11:
+            //     shakeEffect.Play();
+            //     tempTimer = curTimer;
+            //     timerState = TimerState.Start;
+            //     break;
         }
     }
 }
