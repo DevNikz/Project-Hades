7c05e7d20d4cb3ca9bbf67a4c3a487421f1f4387
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2024-11-12 15:12:12 +0800

    updating augment reward menu

--- Assets/Scripts/ScriptableObjects/Overhaul/AugmentScriptable.cs
+++ Assets/Scripts/ScriptableObjects/Overhaul/AugmentScriptable.cs
@@ -17,7 +17,8 @@ public class AugmentScriptable : ScriptableObject
     [VerticalGroup("Attributes/base/left")]
     [BoxGroup("Attributes/base/left/box1")]
     [PreviewField(125, Alignment = ObjectFieldAlignment.Center, FilterMode = FilterMode.Point)] 
-    [HideLabel] [Title("Icon", TitleAlignment = TitleAlignments.Centered, HorizontalLine = false, Bold = false)] public Texture2D augmentIcon;
+    [HideLabel] [Title("Icon", TitleAlignment = TitleAlignments.Centered, HorizontalLine = false, Bold = false)] 
+    public Sprite augmentIcon;
     
     [VerticalGroup("Attributes/base/right")]
     [BoxGroup("Attributes/base/right/box2", showLabel: false)]
