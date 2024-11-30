90d1438e5f4d89dee230367321649f430a8bb013
EJ Taylan <emmanuel_john_taylan@dlsu.edu.ph>
2024-11-30 19:08:22 +0800

    augment window

--- Assets/Scripts/UI/Menu Scripts/AugmentTextScript.cs
+++ Assets/Scripts/UI/Menu Scripts/AugmentTextScript.cs
@@ -8,30 +8,6 @@ public class AugmentTextScript : MonoBehaviour
     [SerializeField] TextMeshProUGUI nameText;
     [SerializeField] TextMeshProUGUI descText;
 
-    public string hpNameField;
-    public string hpDescField;
-
-    public string atkNameField;
-    public string atkDescField;
-
-    public string defNameField;
-    public string defDescField;
-
-    public string stunNameField;
-    public string stunDescField;
-
-    public string earthNameField;
-    public string earthDescField;
-
-    public string fireNameField;
-    public string fireDescField;
-
-    public string waterNameField;
-    public string waterDescField;
-
-    public string windNameField;
-    public string windDescField;
-
     private void Start()
     {
         nameText.text = "";
