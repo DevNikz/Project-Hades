8f9d708449895e794c09801aae7bbc45a8b608e9
DevNikz <paul_ragudo@dlsu.edu.ph>
2025-05-27 08:29:35 +0800

    Fixed some bugs from jira.

--- Assets/Scripts/Entities/Hazard/HitBoxController.cs
+++ Assets/Scripts/Entities/Hazard/HitBoxController.cs
@@ -5,7 +5,7 @@ public class HitBoxController : MonoBehaviour
     void OnTriggerEnter(Collider other) {
         if(other.CompareTag("Player")) {
             Debug.Log("Hit!");
-            other.GetComponent<PlayerController>().ReceiveDamage(DamageType.Physical, 10f);
+            //other.GetComponent<PlayerController>().ReceiveDamage(DamageType.Physical, 10f);
             other.GetComponent<RevampPlayerStateHandler>().ReceiveDamage(DamageType.Physical, 10f);
         }
     }
