3708d724a530d7bf6468c803cd2e328b1cca380a
EJ Taylan <emmanuel_john_taylan@dlsu.edu.ph>
2025-06-04 12:33:15 +0800

    prelim settings functionality

---
+++ Assets/Scripts/UI/Menu Scripts/SettingsMenuScript.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsMenuScript : MonoBehaviour
+{
+    //ref to slider
+    //ref to mixer
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //slider component
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*func for slider()
+    {
+        slider to mixer shit
+    }
+     */
+}
