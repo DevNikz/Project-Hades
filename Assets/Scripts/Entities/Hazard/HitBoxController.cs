14bbdbc6370c2dfd2ee212d01cf21bff2f559318
Josiah Aviso <150400184+WaywardTaco@users.noreply.github.com>
2025-07-16 19:17:55 +0800

    Update HitBoxController.cs

--- Assets/Scripts/Entities/Hazard/HitBoxController.cs
+++ Assets/Scripts/Entities/Hazard/HitBoxController.cs
@@ -2,11 +2,16 @@ using UnityEngine;
 
 public class HitBoxController : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other) {
-        if(other.CompareTag("Player")) {
-            Debug.Log("Hit!");
-            //other.GetComponent<PlayerController>().ReceiveDamage(DamageType.Physical, 10f);
-            other.GetComponent<RevampPlayerStateHandler>().ReceiveDamage(DamageType.Physical, 10f);
+    void OnTriggerEnter(Collider other)
+    {
+        // if(other.CompareTag("Player")) {
+        //     // Debug.Log("Hit!");
+        //     //other.GetComponent<PlayerController>().ReceiveDamage(DamageType.Physical, 10f);
+        //     other.GetComponent<RevampPlayerStateHandler>().ReceiveDamage(DamageType.Physical, 10f);
+        // }
+        
+        if(other.TryGetComponent<EnemyController>(out var enemy)) {
+            enemy.ReceiveDamage(DamageType.Physical, 10f, 100f, AttackDirection.None, Detain.No, false);
         }
     }
 }
